046ca9110abb7455a8ea3a92549a84ca26e25994
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-08 16:26:19 -0600

    Set up reservations schema and repository

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 import { ConfigModule } from '@app/common/config';
 
@@ -15,4 +15,8 @@ import { ConfigModule } from '@app/common/config';
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
